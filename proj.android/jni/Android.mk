12c2846f79f9ab5f51b185bea6fd9188ee851a06
Kristaps Auzins <kristapsauzins@Kristapss-iMac.local>
2014-12-22 00:58:59 +0200

    some more stuff

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -93,6 +93,8 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AdvancedAnimation.cpp \
                    ../../Classes/InGamePowers.cpp \
                    ../../Classes/Enemy_Bee.cpp \
+                   ../../Classes/PanZoomLayer.cpp \
+                   ../../Classes/WorldMap.cpp \
                    ../../Classes/HelloWorldScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
